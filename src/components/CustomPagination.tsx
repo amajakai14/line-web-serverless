90497dddf4b9d18e68f67528db3efb84725f3602
Means <amajakai14@gmail.com>
2023-01-17 21:37:22 +0900

    delete unnessecary props

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -2,16 +2,12 @@ import Pagination from "react-paginate";
 export type TPagination = {
   dataCount: number;
   perPage: number;
-  //   pageRangeDisplayed: number | undefined;
-  //   marginPagesDisplayed: number | undefined;
   handlePageChange: (cur: { selected: number }) => void;
 };
 const CustomPagination = (props: TPagination) => {
   return (
     <Pagination
       pageCount={Math.ceil(props.dataCount / props.perPage)}
-      //   pageRangeDisplayed={props.pageRangeDisplayed}
-      //   marginPagesDisplayed={props.marginPagesDisplayed}
       onPageChange={props.handlePageChange}
       containerClassName="flex flex-row justify-between gap-1"
       activeClassName="active text-2xl"
