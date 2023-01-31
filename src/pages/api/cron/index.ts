4f828e6d44f2d74c7e734ff270632ca08d9655a7
Means <amajakai14@gmail.com>
2023-01-31 19:37:05 +0900

    cron function

--- src/pages/api/cron/index.ts
+++ src/pages/api/cron/index.ts
@@ -1,26 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { env } from "../../../env/server.mjs";
 
 export default async function handler(
-  _req: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("doing some background work");
-  if (!prisma) {
-    res.status(404);
-    console.log("no prisma found");
-    return res.status(404);
+  if (req.method === "POST") {
+    try {
+      const { authorization } = req.headers;
+
+      if (authorization === `Bearer ${env.CRON_SECRET}` && prisma) {
+        await prisma.channel.updateMany({
+          data: { status: "EXPIRED" },
+          where: { time_end: { lte: new Date() } },
+        });
+        res.status(200).json({ success: true });
+      }
+    } catch (err: any) {
+      res.status(500).json({ success: false, message: err.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
-  const result = await prisma.channel.updateMany({
-    data: { status: "EXPIRED" },
-    where: { time_end: { lte: new Date() } },
-  });
-  res.status(200).json({ count: result.count });
 }
-
-/**
- * verifySignature will try to load `QSTASH_CURRENT_SIGNING_KEY` and `QSTASH_NEXT_SIGNING_KEY` from the environment.
-
- * To test out the endpoint manually (wihtout using QStash), you can do `export default handler` instead and
- * hit this endpoint via http://localhost:3000/api/cron
- */
-// export default verifySignature(handler);
