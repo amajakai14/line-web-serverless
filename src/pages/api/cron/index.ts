31d27db09a1899c60c7d306b7eca1456e3990970
Means <amajakai14@gmail.com>
2023-02-01 15:33:51 +0900

    cron fix prisma undefined

--- src/pages/api/cron/index.ts
+++ src/pages/api/cron/index.ts
@@ -1,28 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { env } from "../../../env/server.mjs";
+import { prisma } from "../../../server/db";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    try {
-      const { authorization } = req.headers;
-      console.log("header ", authorization);
-
-      if (authorization === `Bearer ${env.CRON_SECRET}` && prisma) {
-        await prisma.channel.updateMany({
-          data: { status: "EXPIRED" },
-          where: { time_end: { lte: new Date() } },
-        });
-        res.status(200).json({ success: true });
-      }
-      res.status(403).json({ success: false});
-    } catch (err: any) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
+    return;
+  }
+
+  try {
+    const { authorization } = req.headers;
+    if (authorization !== `Bearer ${env.CRON_SECRET}`) {
+      res.status(403).json({ success: false });
+      return;
+    }
+
+    if (!prisma) {
+      res.status(404).json({ success: false, message: "prisma no found" });
+      return;
+    }
+
+    const data = await prisma.channel.updateMany({
+      data: { status: "EXPIRED" },
+      where: { time_end: { lte: new Date() } },
+    });
+    res.status(200).json({
+      success: true,
+      message: `${data.count} items have been updated`,
+    });
+    return;
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
   }
 }
